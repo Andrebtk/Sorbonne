d6688c5ec2950a3a8858c4afb91182a33a032372
Olivier Huang <81701540+HuangOlivier@users.noreply.github.com>
2025-04-20 01:35:47 +0200

    Update test_mem.c

--- S2/LU2IN006/projet/test_mem.c
+++ S2/LU2IN006/projet/test_mem.c
@@ -1,17 +1,81 @@
-#include "memory.h"
-#include "parser.h"
 #include <stdio.h>
+#include "hash.h"
+#include "memory.h" 
+#include "parser.h"
 
+void menu() {
+	printf("\nOption:\n");
+	printf("0 - Initialiser la mémoire\n");
+	printf("1 - Créer un segment \n");
+	printf("2 - Supprimer un segment \n");
+    printf("3 - Afficher le gestionnaire de mémoire\n");
+	printf("4 - Quitter\n");
+}
 
 int main() {
-    HashMap *hp = hashmap_create();
-    ParserResult *p = parse("assembler.txt");
+    MemoryHandler *handler=NULL;
+    int action;
+    char name[100];
+    do {
+        menu();
+        printf("entrée une action:\n");
+        scanf("%d",&action);
+		switch (action){
+            case 0: {
+                printf("Choisissez la taille de votre gestionnaire de mémoire\n");
+                int taille;
+                scanf("%d",&taille);
+                handler = memory_init(taille);
+                break;
+            }
+            case 1: {
+                int start,size;
+                if(handler==NULL){
+                    printf("\nVeuillez d'abord initialiser le gestionnaire de mémoire\n");
+                    break;
+                }else{
+                    printf("Veuillez choisir le nom, start et taille de votre segment(séparée par un espace)\n");
+                    scanf(" %s %d %d", name, &start, &size);
+                    create_segment(handler,name,start,size);
+                    break;
+                }
+            }
+            case 2: {
 
-    
-    resolve_constants(p);
-    //afficher_ParserResult(p);
+                printf("Veuillez entrer le nom du segment\n");
+                scanf("%s",name);
+                remove_segment(handler, name);
+                break;
+            }
+            case 3:{ print_memory(handler);
+                break;
+            }
+            case 4: {
+                if(handler){
+                    free_memoryHandler(handler);
+                }
+            }
+        }
 
+    } while(action!=4);
+    /*
+    // Initialisation du gestionnaire de mémoire
+    MemoryHandler *handler= memory_init(1024);
 
-    free_ParserResult(p);
-	free_HashMap(hp);
+    printf("\nCréation de segments mémoire\n");
+    create_segment(handler, "test1", 100, 50);
+    create_segment(handler, "test2", 200, 75);
+    create_segment(handler, "test3", 500, 100);
+
+    printf("\nAffichage de la mémoire\n");
+    print_memory(handler);
+    printf("\n");
+
+    printf("\nAffichage après uppression de test2\n");
+    remove_segment(handler, "test2");
+    print_memory(handler);
+    printf("\n");
+    free_memoryHandler(handler);
+ */   
+    return 0;
 }
