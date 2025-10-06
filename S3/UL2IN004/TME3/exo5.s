af844717a22225bac968ef14a364c805d61012ba
Andre Bertok <Andre.Bertok@etu.sorbonne-universite.fr>
2025-10-06 22:02:50 +0200

    :construction: TME3 Archi

--- S3/UL2IN004/TME3/exo5.s
+++ S3/UL2IN004/TME3/exo5.s
@@ -1,6 +1,7 @@
 .data 
 
 .text	
+	# Partie 1/3
 	# Met 0xAABBCCDD dans $3 
 	ori $3, $0, 0xAABB
 	sll $3, $3, 16			# Shift left pour apres mettre le reste des valeur dans les bit restant
@@ -16,7 +17,7 @@
 	ori $2, $0, 11
 	syscall
 	
-	# Partie 2
+	# Partie 2/3
 	ori $3, $0, 0xABCD 		# objectif $5=0xDBAC	
 
 	ori $8, $0, 0xF000		# Masque pour le bit 3
@@ -51,6 +52,32 @@
 	# Affiche '\n'	
 	ori $4, $0, 0x0A 		# code ASCII pour '\n' (LF)
 	ori $2, $0, 11
+	syscall
+	
+	
+	# Sur 32 bit - Partie 3/3
+	lui $23, 0xAABB 		
+	ori $23, $23, 0xCCDD		# objectif $5=0xDDBBAACC	
+	
+	
+	andi $16, $23, 0xFF000000	# Masque -> recupere les bits 3 dans $16
+	andi $17, $23, 0x00FF0000	# Masque -> recupere les bits 2 dans $17
+	andi $18, $23, 0x0000FF00	# Masque -> recupere les bits 1 dans $18
+	andi $19, $23, 0x000000FF	# Masque -> recupere les bits 0 dans $19
+	
+	srl $16, $16, 16		# Shift R -> on a 0x0000aa00
+	srl $18, $18, 8			# Shift R -> on a 0x000000cc
+	sll $19, $19, 24		# Shift L -> on a 0xdd000000
+	
+	# On reconstruie la valeur final
+	or $5, $17, $16			# 0x00bbaa00
+	or $5, $5, $18			# 0x00bbaacc
+	or $5, $5, $19			# 0xddbbaacc
+	
+	
+	# Affiche $5 a l'ecran
+	or $4, $0, $5
+	ori $2, $0, 34
 	syscall
 	
 
